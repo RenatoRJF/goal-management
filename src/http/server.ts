a002cf86f0e3001753cfc4cc08c580a17854a5a2
Renato Ferreira <renatoferreira@Renatos-MacBook-Pro.local>
2024-09-12 09:10:45 +0200

    feat(TT-BE): create goal route and implement create goal

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,33 @@
+import z from 'zod'
 import fastfy from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { createGoal } from '../services/create-goal'
 
-const app = fastfy()
+const app = fastfy().withTypeProvider<ZodTypeProvider>()
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.post(
+  '/goals',
+  {
+    schema: {
+      body: z.object({
+        title: z.string(),
+        desiredWeeklyFrequency: z.number().int().min(1).max(7),
+      }),
+    },
+  },
+  async ({ body }) => {
+    const { title, desiredWeeklyFrequency } = body
+
+    await createGoal({ title, desiredWeeklyFrequency })
+  }
+)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('Server running on port 3333')
